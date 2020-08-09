829bef236a4168046d5871be29e9b8f4a8af368e
matuken_3 <k.matutaka.biz@gmail.com>
2020-08-10 08:55:02 +0900

    create todolist

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <v-toolbar dark color="indigo"></v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <v-toolbar color="indigo" dark>
+      <v-toolbar-title class="white--text">My Todo</v-toolbar-title>
+    </v-toolbar>
+    <v-list>
+      <template v-for="(todo, index) in todos">
+        <v-list-item v-bind:key="index">
+          <v-list-item-content>{{todo}}</v-list-item-content>
+          <v-list-item-action>
+            <v-btn icon>
+              <v-icon @click="deleteTodo(index)">mdi-delete</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+        <v-divider v-bind:key="`divider-${index}`"></v-divider>
+      </template>
+    </v-list>
+    <div class="bottom-right">
+      <v-btn fab color="indigo">
+        <v-icon color="white" @click="addTodo()">mdi-plus</v-icon>
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: ["hoge", "moge"],
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      console.log("delete todo:" + id);
+    },
+    addTodo() {
+      console.log("add todo");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.bottom-right {
+  position: fixed;
+  bottom: 0px;
+  right: 0px;
+}
+</style>
